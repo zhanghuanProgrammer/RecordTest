b4ff2b02c04d66f0127f9aef2e39440edcabe696
张欢 <1213478506@qq.com>
2018-04-18 16:04:33 +0800

    temp

--- v1.0实现框架搭建/RecordTest/VCPath/RTSearchVCPath.h
+++ v1.0实现框架搭建/RecordTest/VCPath/RTSearchVCPath.h
@@ -1,5 +1,6 @@
 
 #import <Foundation/Foundation.h>
+#import "RTOperationQueue.h"
 
 @interface RTSearchVCPath : NSObject
 
@@ -7,6 +8,8 @@
 @property (nonatomic,weak)UIViewController *curVC;
 
 @property (nonatomic,assign)BOOL isLearnVCPath;
+
++ (void)addOperation:(UIView *)view type:(RTOperationQueueType)type parameters:(NSArray *)parameters repeat:(BOOL)repeat;
 
 - (BOOL)canGoToVC:(NSString *)vc;
 - (void)goToVC:(NSString *)vc;
